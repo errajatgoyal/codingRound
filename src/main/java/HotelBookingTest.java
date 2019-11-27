00c65f71bae430724d52b350b6262690d2af1cb8
errajatgoyal <alpha17495@gmail.com>
2019-11-27 19:50:52 +0530

    [CodingRound] - HotelBookingTest
    Used @BeforeTest to use Chrome Options to disable notifications to handle the popup asking permission to show notifications, initialize Chrome driver and setting implicit wait to set a wait across all elements
    - Used @AfterTest to close the browser after every test
    - Used PageFactory class's initElements() function to initialize the Web Elements specified under @FindBy tag

--- src/main/java/HotelBookingTest.java
+++ src/main/java/HotelBookingTest.java
@@ -2,13 +2,19 @@ import com.sun.javafx.PlatformUtil;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.Select;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
 
 public class HotelBookingTest {
 
-    WebDriver driver = new ChromeDriver();
+    WebDriver driver;
 
     @FindBy(linkText = "Hotels")
     private WebElement hotelLink;
@@ -22,32 +28,43 @@ public class HotelBookingTest {
     @FindBy(id = "travellersOnhome")
     private WebElement travellerSelection;
 
+
+    @BeforeTest
+    public void beforeTest() {
+        setDriverPath();
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.addArguments("--disable-notifications");
+        chromeOptions.addArguments("--start-maximized");
+        driver = new ChromeDriver(chromeOptions);
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+    }
+
+    @AfterTest
+    public void afterTest() {
+        driver.quit();
+    }
+
     @Test
     public void shouldBeAbleToSearchForHotels() {
-        setDriverPath();
 
+        PageFactory.initElements(driver, this);
         driver.get("https://www.cleartrip.com/");
+
         hotelLink.click();
-
         localityTextBox.sendKeys("Indiranagar, Bangalore");
-
         new Select(travellerSelection).selectByVisibleText("1 room, 2 adults");
         searchButton.click();
-
-        driver.quit();
-
     }
 
     private void setDriverPath() {
         if (PlatformUtil.isMac()) {
             System.setProperty("webdriver.chrome.driver", "chromedriver");
         }
-        if (PlatformUtil.isWindows()) {
+        if (PlatformUtil.isWin7OrLater()) {
             System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
         }
         if (PlatformUtil.isLinux()) {
             System.setProperty("webdriver.chrome.driver", "chromedriver_linux");
         }
     }
-
 }
