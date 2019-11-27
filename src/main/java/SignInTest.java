401427e3cb9a2767e1f89ea782ced33c37832ea1
errajatgoyal <alpha17495@gmail.com>
2019-11-27 19:45:18 +0530

    [CodingRound] SignInTest
    - Used @BeforeTest to use Chrome Options to disable notifications to handle the popup asking permission to show notifications, initialize Chrome driver and setting implicit wait to set a wait across all elements
    - Used @AfterTest to close the browser after every test
    - Commented out the 'waitFor()' statement and handled it through implicit wait
    - used driver.switch to move to the modal window where the rest of the execution needed to happen

--- src/main/java/SignInTest.java
+++ src/main/java/SignInTest.java
@@ -2,9 +2,13 @@ import com.sun.javafx.PlatformUtil;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
+import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
 
 public class SignInTest {
 
@@ -13,23 +17,31 @@ public class SignInTest {
     @BeforeTest
     public void beforeTest() {
         setDriverPath();
-        driver = new ChromeDriver();
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.addArguments("--disable-notifications");
+        chromeOptions.addArguments("--start-maximized");
+        driver = new ChromeDriver(chromeOptions);
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+    }
+
+    @AfterTest
+    public void afterTest() {
+        driver.quit();
     }
 
     @Test
-    public void shouldThrowAnErrorIfSignInDetailsAreMissing() {
+    public void shouldThrowAnErrorIfSignInDetailsAreMissing() throws InterruptedException {
         driver.get("https://www.cleartrip.com/");
-        waitFor(2000);
+        //  waitFor(2000);
 
         driver.findElement(By.linkText("Your trips")).click();
         driver.findElement(By.id("SignIn")).click();
-
+        driver.switchTo().frame("modal_window");
         driver.findElement(By.id("signInButton")).click();
-
         String errors1 = driver.findElement(By.id("errors1")).getText();
         Assert.assertTrue(errors1.contains("There were errors in your submission"));
-        driver.quit();
     }
+
 
     private void waitFor(int durationInMilliSeconds) {
         try {
